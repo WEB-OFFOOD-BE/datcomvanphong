3c3b264d929a99c6d1e71c0aa13a879eb849050a
QuangLB14 <quanglb14@gmail.com>
2022-08-16 00:28:57 +0700

    update avatar for staff

--- src/main/java/com/example/websitedatmon/controllers/EmployeeController.java
+++ src/main/java/com/example/websitedatmon/controllers/EmployeeController.java
@@ -4,14 +4,18 @@ import com.example.websitedatmon.entity.Role;
 import com.example.websitedatmon.entity.User;
 import com.example.websitedatmon.serviceImpls.RoleServiceImpl;
 import com.example.websitedatmon.serviceImpls.UserServiceImpl;
+import com.example.websitedatmon.utils.FileUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
 import java.util.List;
 
 @Controller
@@ -34,21 +38,28 @@ public class EmployeeController {
     }
 
     @PostMapping(value = "/employee-add")
-    public ModelAndView add(HttpServletRequest request){
+    public ModelAndView add(HttpServletRequest request, @RequestParam("file") MultipartFile image){
         ModelAndView mv = new ModelAndView("redirect:employee");
         String fullname = request.getParameter("fullname");
         String email = request.getParameter("email");
         String sdt = request.getParameter("sdt");
-        String taikhoan = request.getParameter("taikhoan");
+        String username = request.getParameter("taikhoan");
         String password =request.getParameter("matkhau");
         User user = new User();
         user.setEmail(email);
         user.setFullName(fullname);
         user.setPhoneNumber(sdt);
-        user.setUserName(taikhoan);
+        user.setUserName(username);
         user.setPassword(password);
         Role role = roleService.findRoleById(3);
-        user.setRole(role);
+        user.setRoleId(3);
+        String fileName = "";
+        try {
+            fileName = FileUtil.upload(image,request);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        user.setImage(fileName);
         userService.save(user);
         mv.addObject("msg","success");
         return mv;
