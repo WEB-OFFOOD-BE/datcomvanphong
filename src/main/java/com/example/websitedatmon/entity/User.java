d05e6124f3e0769712fcdf69b5994afa8cd29c29
ThiKV <thikvph12942@fpt.edu.vn>
2022-08-27 15:04:53 +0700

    fix user

--- src/main/java/com/example/websitedatmon/entity/User.java
+++ src/main/java/com/example/websitedatmon/entity/User.java
@@ -1,16 +1,14 @@
 package com.example.websitedatmon.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.*;
 import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
-@Data
+@Getter
+@Setter
 @Builder
 @Table(name = "user")
 @Entity
@@ -45,4 +43,64 @@ public class User {
     @ManyToOne
     @JoinColumn(name = "role_id", referencedColumnName = "id", insertable = false,updatable = false)
     private Role role;
+
+    public List<Orders> getOrders() {
+        return orders;
+    }
+
+    public List<Feedback> getFeedbacks() {
+        return feedbacks;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setOrders(List<Orders> orders) {
+        this.orders = orders;
+    }
+
+    public void setFeedbacks(List<Feedback> feedbacks) {
+        this.feedbacks = feedbacks;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public int getRoleId() {
+        return roleId;
+    }
+
+    public Integer getIsActive() {
+        return isActive;
+    }
 }
