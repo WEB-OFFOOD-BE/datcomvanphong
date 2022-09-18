efc96819a7cac61bc9f810b65ee74e17b2b065d2
ThiKV <thikvph12942@fpt.edu.vn>
2022-09-18 11:02:08 +0700

    update

--- src/main/java/com/example/websitedatmon/model/HistoryResponse.java
+++ src/main/java/com/example/websitedatmon/model/HistoryResponse.java
@@ -2,13 +2,31 @@ package com.example.websitedatmon.model;
 
 import com.example.websitedatmon.entity.Orders;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 @Data
 public class HistoryResponse {
-    Orders order;
-    Boolean isToday;
+    public Orders order;
+    public Boolean isToday;
+
+    public Orders getOrder() {
+        return order;
+    }
+
+    public void setOrder(Orders order) {
+        this.order = order;
+    }
+
+    public Boolean getIsToday() {
+        return isToday;
+    }
+
+    public void setToday(Boolean today) {
+        isToday = today;
+    }
 }
